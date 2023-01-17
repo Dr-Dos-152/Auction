d3bb3b21a28a923a9a276dfca07dace624de3a80
Shubham Dhingra <dhingra.shubham38@gmail.com>
2023-01-17 15:06:58 +0000

    Scroll to bottom

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,37 +1,64 @@
 import { SpaceBetween } from '@cloudscape-design/components';
 import Button from '@cloudscape-design/components/button';
 import Input from '@cloudscape-design/components/input';
+import { noop } from 'lodash';
 import moment from 'moment';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { KeyCodes } from '../constants/keyCodes';
 import styles from "../styles/Chat.module.scss";
 
 const Chat = (props: {
   userName: string, messages: Array<ChatMessageResponse>, publishMessage: (message: { destinationUserName: string; body: string }) => void,
 }) => {
   const [message, setMessage] = useState("");
+  const bottomDiv = useRef<null | HTMLDivElement>(null)
+
+  useEffect(() => {
+    setMessage("")
+  }, [props.userName])
+
+  /**
+   * TODO: Not ideal, but will work for now
+   */
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      bottomDiv.current?.scrollIntoView(
+        {
+          behavior: 'smooth',
+          block: 'end',
+          inline: 'nearest'
+        })
+    }, 500)
+  }, [props.messages])
+
 
   const handleClick = () => {
     props.publishMessage({
       destinationUserName: props.userName,
       body: message
     })
+    setMessage("")
   }
 
-  const messages = props.messages ? props.messages.map(message => <Message {...message} />) : "No messages";
+  const messages = props.messages ? props.messages.map(message => <Message {...message} highlight={message.userName !== props.userName} />) : "No messages";
 
   return (
     <div>
       <SpaceBetween size={'s'} direction="vertical">
         <div className={styles.chatbox}>
           {messages}
+          <div
+            ref={bottomDiv}>
+          </div>
         </div>
+
 
         <div style={{
           display: 'flex',
           gap: "1rem"
         }}>
           <div style={{ width: "70%" }}>
-            <Input value={message} onChange={(e) => setMessage(e.detail.value)} />
+            <Input onKeyDown={(e) => e.detail.keyCode === KeyCodes.ENTER_KEY ? handleClick() : noop} placeholder='Type something...' value={message} onChange={(e) => setMessage(e.detail.value)} />
           </div>
           <Button onClick={() => handleClick()}>Send</Button>
         </div>
@@ -41,20 +68,24 @@ const Chat = (props: {
 }
 
 
-const Message = (props: ChatMessageResponse) => {
+const Message = (props: ChatMessageResponse & { highlight: boolean }) => {
 
 
   return (
-    <div className={styles.message}>
-      <span>
-        {moment(props.dateTime).format('HH:mm:ss')}
-      </span>
-      <span className={styles.messageUserName}>
-        {props.userName}:
-      </span>
-      <span className={styles.messageContent}>
-        {props.message}
-      </span>
+    <div className={props.highlight ? styles.messageContainer : ""}>
+      <div className={props.highlight ? styles.messageHighlight : styles.message}>
+        <div className={styles.messageHeader}>
+          <span>
+            {moment(props.dateTime).format('HH:mm:ss')}
+          </span>
+          <span className={props.highlight ? styles.messageUserNameHighlight : styles.messageUserName}>
+            {props.userName}
+          </span>
+        </div>
+        <span className={styles.messageContent}>
+          {props.message}
+        </span>
+      </div>
     </div>
   )
 }
